ba97bdb61c60c18e35f141f4ba2692d803d5d29b
davidpaley <vpndavidp@city-furniture.com>
2021-06-04 09:51:36 -0300

    improving the code

--- main.js
+++ main.js
@@ -18,45 +18,24 @@ const chromeLauncher = require("chrome-launcher");
         "largest-contentful-paint",
         "total-blocking-time",
         "interactive",
+        "cumulative-layout-shift",
       ],
     },
   };
-
-  const fcpArr = [];
-  const siArr = [];
-  const lcpArr = [];
-  const tbtArr = [];
-  const iArr = [];
-
   let fcp = 0;
   let si = 0;
   let lcp = 0;
   let tbt = 0;
   let interactive = 0;
+  let cumulativeLayoutShift = 0;
   let score = 0;
 
-  const number_of_runs = 5;
+  const number_of_runs = 1;
   for (let i = 0; i < number_of_runs; i++) {
     const runnerResult = await lighthouse(
-      "http://web-main-git-cfc-11911-improve-browse-mobile-list-4af713.vercel.app/browse/bedroom/beds",
+      "https://web-main-bfnh7zbhp-city-furniture.vercel.app/product/9719211/hilton-two-tone-96-table-4-skirted-chairs",
       options
     );
-
-    // `.report` is the HTML report as a string
-    // const reportHtml = runnerResult.report;
-    // fs.writeFileSync("lhreport.json", reportHtml);
-    console.log({
-      fcp: runnerResult.lhr.audits["first-contentful-paint"].numericValue,
-      domSize: runnerResult.lhr.audits["dom-size"].numericValue,
-    });
-    fcpArr.push(runnerResult.lhr.audits["first-contentful-paint"].numericValue);
-    siArr.push(runnerResult.lhr.audits["speed-index"].numericValue);
-    lcpArr.push(
-      runnerResult.lhr.audits["largest-contentful-paint"].numericValue
-    );
-    tbtArr.push(runnerResult.lhr.audits["total-blocking-time"].numericValue);
-    iArr.push(runnerResult.lhr.audits["interactive"].numericValue);
-
     fcp = fcp + runnerResult.lhr.audits["first-contentful-paint"].numericValue;
     si = si + runnerResult.lhr.audits["speed-index"].numericValue;
     lcp =
@@ -64,6 +43,14 @@ const chromeLauncher = require("chrome-launcher");
     tbt = tbt + runnerResult.lhr.audits["total-blocking-time"].numericValue;
     interactive =
       interactive + runnerResult.lhr.audits["interactive"].numericValue;
+    cumulativeLayoutShift =
+      cumulativeLayoutShift +
+      runnerResult.lhr.audits["cumulative-layout-shift"].numericValue;
+    console.log({
+      score: `=================${
+        runnerResult.lhr.categories.performance.score * 100
+      }==============================`,
+    });
     score = score + runnerResult.lhr.categories.performance.score * 100;
   }
 
@@ -72,21 +59,17 @@ const chromeLauncher = require("chrome-launcher");
   lcp = lcp / number_of_runs;
   tbt = tbt / number_of_runs;
   interactive = interactive / number_of_runs;
+  cumulativeLayoutShift = cumulativeLayoutShift / number_of_runs;
   score = score / number_of_runs;
-
   // `.lhr` is the Lighthouse Result as a JS object
   // console.log("Report is done for", runnerResult.lhr.finalUrl);
-  console.log(
-    "Performance score was",
-    score
-    // runnerResult.lhr.categories.performance.score * 100
-  );
   console.log({
     fcp,
     si,
     lcp,
     tbt,
     interactive,
+    cumulativeLayoutShift,
     score,
   });
 
